6c9f9fbb595e836aa042219210ff086d96a9ca45
Antonio Guedes <gguedesantonio@gmail.com>
2020-08-01 13:46:26 +1000

    Notes route created

--- server.js
+++ server.js
@@ -18,9 +18,13 @@ app.use(express.json());
 // Routes
 // =============================================================
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
   });
+
+app.get("/notes", (req, res) => {
+res.sendFile(path.join(__dirname, "public//notes.html"));
+});
 
 
 
