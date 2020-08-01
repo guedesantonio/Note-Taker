969f7231477962b0080e4eb052d373d917389162
Antonio Guedes <gguedesantonio@gmail.com>
2020-08-01 14:48:01 +1000

    Notes GET request fixed

--- server.js
+++ server.js
@@ -14,46 +14,26 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
-// =============================================================
-
 // Routes
 // =============================================================
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
 
 app.get("/notes", (req, res) => {
-res.sendFile(path.join(__dirname, "public/notes.html"));
+res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-// Displays all notes
+// Api routes
 app.get("/api/notes", (req, res) => {
-    fs.readFile(__dirname, "db/db.json", "utf8", function(error, data) {
+    fs.readFile(__dirname, "./db/db.json", "utf8", function(error, data) {
         if (error) {
           return console.log(error);
         }
-        return res.json(data);
+        return res.json(JSON.parse(data));
       });
   });
-
-  app.post("/api/characters", (req, res) => {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    const newCharacter = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-  
-    console.log(newCharacter);
-  
-    characters.push(newCharacter);
-  
-    res.json(newCharacter);
-  });
-
 
 
 
