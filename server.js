7de0583e85ad2c348656623f9e1b8a56e5e3236a
Antonio Guedes <gguedesantonio@gmail.com>
2020-08-01 13:44:16 +1000

    Index route created

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
+// Dependencies
+// =============================================================
 const express = require("express");
 const path = require("path");
 
+// Sets up the Express App
+// =============================================================
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 
+// Routes
+// =============================================================
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
 
 
 
 
+
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
   });
